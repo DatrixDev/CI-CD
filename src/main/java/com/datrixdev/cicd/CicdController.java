a5c119fbcc2f9eefaaf365195f8eb614a0bb11e9
quocdat233 <datvq.24it@vku.udn.vn>
2026-09-25 14:12:22 +0700

    Update to version 2

--- src/main/java/com/datrixdev/cicd/CicdController.java
+++ src/main/java/com/datrixdev/cicd/CicdController.java
@@ -10,7 +10,7 @@ public class CicdController {
     public Map<String, Object> cicd() {
         return Map.of(
                 "status",200,
-                "message","CI-CD Spring Boot"
+                "message","BỤI DJ"
         );
     }
 
