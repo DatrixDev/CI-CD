32a0b67e892a8b3832c26507b614f24819e6b815
quocdat233 <datvq.24it@vku.udn.vn>
2026-09-26 23:16:05 +0700

    feat: add production environment configuration

--- src/main/java/com/datrixdev/cicd/CicdController.java
+++ src/main/java/com/datrixdev/cicd/CicdController.java
@@ -1,16 +1,28 @@
 package com.datrixdev.cicd;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 @RestController
 public class CicdController {
+
+//    @GetMapping("/ci-cd")
+//    public Map<String, Object> cicd() {
+//        return Map.of(
+//                "status",200,
+//                "message", "Datcute"
+//        );
+//    }
+
+    @Value("${app.message}")
+    private String message;
     @GetMapping("/ci-cd")
     public Map<String, Object> cicd() {
         return Map.of(
                 "status",200,
-                "message", "Datcute"
+                "message", message
         );
     }
 
